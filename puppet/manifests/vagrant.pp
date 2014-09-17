3e3a2262b527b1576ea4ef9bb2ad8c5e3bdcbb10
justgoodman <justgoodman@yandex.ru>
2014-09-17 17:04:12 +0300

    change gap formet

--- puppet/manifests/vagrant.pp
+++ puppet/manifests/vagrant.pp
@@ -4,10 +4,11 @@ package {
     'vim': ensure => installed;
 }
 
-class { 'redis':
-    max_memory => '64mb',
-    memory_policy => 'allkeys-lru',
-    snapshotting => {};
+class {
+    'redis':
+        max_memory => '64mb',
+        memory_policy => 'allkeys-lru',
+        snapshotting => {};
     'memcached':
-        listen => '0.0.0.0'
+        listen => '0.0.0.0';
 }
