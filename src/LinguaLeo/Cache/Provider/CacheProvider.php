010b3e11bb5555535af90be109e30812ff028d80
justgoodman <justgoodman@yandex.ru>
2014-09-25 13:59:01 +0300

    remove extra method

--- src/LinguaLeo/Cache/Provider/CacheProvider.php
+++ src/LinguaLeo/Cache/Provider/CacheProvider.php
@@ -91,27 +91,4 @@ abstract class CacheProvider implements CacheInterface
         }
         return $result;
     }
-    /**
-    * Generate cache keys.
-    * The last parameter must be an array
-    * @return array
-    * @throws \InvalidArgumentException
-    */
-    public static function generateCacheKeysArray()
-    {
-        $args = func_get_args();
-        if (empty($args)) {
-            throw new \InvalidArgumentException('At least one argument must be passed to generate cache key.');
-        }
-        $ids = array_pop($args);
-        if (!is_array($ids)) {
-            throw new \InvalidArgumentException('The last parameter must be an array.');
-        }
-        $commonKey = call_user_func_array(['self', 'generateCacheKey'], $args);
-        $result = [];
-        foreach ($ids as $id) {
-            $result[] = $commonKey . self::$delimiter . $id;
-        }
-        return $result;
-    }
 }
